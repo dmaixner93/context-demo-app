08f8b0ef6aea057477208c6f35ca92ebc8895046
daniel <dmaixner93@gmail.com>
2020-05-17 14:04:56 -0500

    Implemented ThemeContext for DarkMode

---
+++ src/PageContent.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { ThemeContext } from './context/ThemeContext';
+
+class PageContent extends Component {
+    static contextType = ThemeContext;
+    render() {
+        const { isDarkMode } = this.context;
+        const styles = {
+            backgroundColor: isDarkMode ? "#121217" : "#FAFAFA",
+            height: "100vh",
+            width: "100vw"
+        }
+        return (
+            <div style={styles}>{this.props.children}</div>
+        )
+    }
+}
+
+export default PageContent;
